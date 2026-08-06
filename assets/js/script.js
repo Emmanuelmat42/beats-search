3eedc8d3d6f05cd3cb44802a32aa93d0b552b2a6
Matis Production <emmanuelmat42@gmail.com>
2026-08-06 19:09:22 +0200

    Suppression de l'animation des beats

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,30 +27,6 @@ window.addEventListener("scroll", () => {
 
 });
 
-// =======================================
-// APPARITION DES SECTIONS
-// =======================================
-
-const hiddenElements = document.querySelectorAll(".hidden");
-
-const observer = new IntersectionObserver((entries) => {
-
-    entries.forEach(entry => {
-
-        if (entry.isIntersecting) {
-
-            entry.target.classList.add("show");
-
-        }
-
-    });
-
-}, {
-    threshold: 0.05
-});
-
-hiddenElements.forEach(el => observer.observe(el));
-
 
 // =======================================
 // RETOUR EN HAUT
